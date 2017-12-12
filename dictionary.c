fadbade18d3b05c6f0f4097ff6d3359f3d686b08
Alexander Valkov <none@nowhe.re>
2017-12-12 03:46:51 +0300

    added hashtable, opened dict, iteration through dict: allocating space for new node, strcpy

--- dictionary.c
+++ dictionary.c
@@ -4,6 +4,17 @@
 
 #include "dictionary.h"
 
+#define HTSIZE 26
+
+type struct _node 
+{
+    char word[LENGTH + 1];
+    struct _node* next;
+}
+node;
+
+node* hashtable[HTSIZE];
+
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
@@ -14,7 +25,41 @@ bool check(const char *word)
 // Loads dictionary into memory, returning true if successful else false
 bool load(const char *dictionary)
 {
-    // TODO
+    // properly initialize hashtable to NULL
+    for ( i = 0; i < HTSIZE; i++ )
+        hashtable[i] = NULL;
+
+    // open dictionary file
+    FILE *dictptr = fopen(dictionary, "r");
+    if (dictptr == NULL)
+    {
+        fprintf(stderr, "Could not open %s.\n", dictionary);
+        return false;
+    }
+
+    while ( fscanf(dictptr, "%s", word) != EOF )
+    {
+        // allocate space for a new node, error check
+        node* new_node = malloc(sizeof(node));
+        if ( new_node == NULL )
+        {
+            unload();
+            return false;
+        }
+
+        // copy word into node
+        strcopy(new_node->word, word);
+        // set next to NULL, to avoid pointing to garbage values
+        new_node->next = NULL;
+
+        // hash TODO
+        
+        // put into hashtable
+
+
+    }
+
+    fclose(dictptr);
     return false;
 }
 
